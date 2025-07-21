5c16e99bf4ac7015c249f6c0f9733428b2da706f
younessaitali <younes.aitali95@gmail.com>
2025-07-21 23:10:05 +0100

    feat: add experimental websocket support in nitro configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/ui', '@pinia/nuxt', '@vueuse/nuxt'],
+  nitro: {
+    experimental: {
+      websocket: true
+    }
+  }
 })
